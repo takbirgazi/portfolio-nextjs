f3270378265412eace01d514923b6c88d30c5cc5
takbirgazi <takbirgazibd@gmail.com>
2024-11-26 23:57:58 +0600

    Update:Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,6 @@
 "use client"
 
 import Image from "next/image";
-import androidAppImg from "@/public/assets/images/downloadApp.png";
 
 const Footer = () => {
     return (
@@ -11,14 +10,14 @@ const Footer = () => {
                     <p className="text-center md:text-left">&copy; {new Date().getFullYear()} All Right Are Resurved by Md. Takbir Gazi</p>
                 </div>
                 <div className="flex items-center justify-center">
-                    <a href="../../public/assets/appFile/portfolio.apk" download>
+                    <a href="https://drive.google.com/file/d/1w3PnyqqRRbK2wNtOsS0F5F-w4QD6tiHh/view?usp=sharing" download>
                         <figure className="h-10 w-36 border rounded-md border-gray-300 overflow-hidden">
                             <Image className="h-full w-full" src={androidAppImg} alt="Download App" />
                         </figure>
                     </a>
                 </div>
             </div>
-        </div> 
+        </div>
     );
 };
 
